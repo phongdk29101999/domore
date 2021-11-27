bd52d6630d991b1c4bf836318512bc4506cb5384
HuongDT <huongdt@caerux.com>
2021-11-27 23:24:43 +0700

    page 2 page 3

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -38,7 +38,7 @@
     </div>
 </div>
 <!-- Preloader Start-->
-<main class="login-body" data-vide-bg="{{asset('/user/img/login-bg.mp4')}}">
+<main class="login-body">
     @yield('content')
 </main>
 
