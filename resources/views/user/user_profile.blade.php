c974066b46b8b3f0f79eaa78dfbf356372bdee6e
thinhle99 <thinhlx99@gmail.com>
2021-11-30 18:53:53 +0700

    profile + edit profile

--- resources/views/user/user_profile.blade.php
+++ resources/views/user/user_profile.blade.php
@@ -11,7 +11,7 @@
                     <div class="row">
                         <div class="col-xl-8 col-lg-11 col-md-12">
                             <div class="hero__caption hero__caption2">
-                                <h1 data-animation="bounceIn" data-delay="0.2s">Thinh</h1>
+                                <h1 data-animation="bounceIn" data-delay="0.2s">Your profile</h1>
                                 <!-- breadcrumb Start-->
                                 <nav aria-label="breadcrumb">
                                     <ol class="breadcrumb">
