389c43c77aa547e4db06313804a34ad5ed818d9b
HuongDT <huongdt@caerux.com>
2021-12-05 23:42:15 +0700

    my question

--- resources/views/post/create_post.blade.php
+++ resources/views/post/create_post.blade.php
@@ -10,15 +10,8 @@
                 <div class="row">
                     <div class="col-xl-8 col-lg-11 col-md-12">
                         <div class="hero__caption hero__caption2">
-                            <h1 data-animation="bounceIn" data-delay="0.2s">Create new blog</h1>
-                            <!-- breadcrumb Start-->
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="{{URL::to('/')}}">Home</a></li>
-                                    <li class="breadcrumb-item"><a href="{{URL::to('/posts')}}">All Blog</a></li>
-                                </ol>
-                            </nav>
-                            <!-- breadcrumb End -->
+                            <h1 data-animation="bounceIn" data-delay="0.2s">あなたの質問 </h1>
+                           
                         </div>
                     </div>
                 </div>
@@ -30,14 +23,14 @@
 <div class="container">
     <div class="row">
         <div class="comment-form">
-            <h4>Your blog</h4>
+            <h4 class="title">あなたの質問</h4>
             <form class="form-contact comment_form" action="{{URL::to('/create_post')}}" id="commentForm" method="post" enctype="multipart/form-data" >
                 {{ csrf_field() }}
                 <div class="row">
 
                     <div class="col-sm-4">
                         <div class="form-group">
-                            <input class="form-control" name="title" id="title" type="text" placeholder="Title">
+                            <input class="form-control" name="title" id="title" type="text" placeholder="タイトル">
                             @error('title')
                             <b><span style="color: red;">{{ $message }}</span></b>
                             @enderror
@@ -45,46 +38,45 @@
                     </div>
 
                     <div class="col-sm-4">
-                        <div class="col-xs-12 col-sm-8">
+                        <div class="col-xs-12 " style="display: flex;">
+                            <img src="{{ asset('/user/img/logo/Vector.png') }}" alt="">
                             <label for="post_url" class="btn btn3 custom-file-upload">
-                                    Upload cover image
+                                画像をアップロード
                             </label>
-
                             <input type="file" name="post_url" class="file-upload" id="post_url">
-                            {{-- <input type="file" name="post_url" id="post_url"> --}}
                         </div>
                         <div class="vspace-12-sm"></div>
                     </div>
+                   
+
 
                     <div class="col-12">
-                        <p>Tags</p>
-                        <div class="form-group">
-                            @foreach($tags as $tag)
-                            <label class="checkbox-inline"><input type="checkbox" name="tags[]" value="{{$tag->tag_id}}">{{$tag->tag_title}}</label>
-                            @endforeach
+                        <select class="form-select form-select-lg" aria-label=".form-select-lg example">
+                        <option selected>科目</option>
+                        @foreach($tags as $tag)  
+                            <option value="{{$tag->tag_id}}">{{$tag->tag_title}}</option>
+                        @endforeach
+                        </select>
                             <br/>
                                 @error('tags')
                                 <b><span style="color: red;">{{ $message }}</span></b>
                                 @enderror
-                        </div>
                     </div>
 
                     <div class="col-12">
                         <div class="form-group">
-                            <textarea class="form-control w-100" name="description" id="comment" cols="30" rows="1" placeholder="Description"></textarea>
+                            <textarea class="form-control w-100" name="description" id="comment" cols="30" rows="1" placeholder="説明"></textarea>
                             @error('description')
                             <b><span style="color: red;">{{ $message }}</span></b>
                             @enderror
                         </div>
                     </div>
 
-
-
                     <div class="col-12">
                         <!-- Nav tabs -->
                         <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#content" aria-controls="content" role="tab" data-toggle="tab">Edit content</a></li>
-                            <li role="presentation"><a href="#preview" aria-controls="preview" role="tab" data-toggle="tab">Preview changes</a></li>
+                            <li role="presentation" class="active"><a href="#content" aria-controls="content" role="tab" data-toggle="tab">コンテンツを編集</a></li>
+                            <li role="presentation"><a href="#preview" aria-controls="preview" role="tab" data-toggle="tab">プレビューの変更</a></li>
                         </ul>
                     </div>
                     <div class="col-12">
@@ -93,7 +85,7 @@
                             {{-- <div class="tab-content"> --}}
                                 <div role="tabpanel" class="tab-pane active" id="content">
                                     <div class="form-group">
-                                        <textarea class="form-control w-100" name="detail_content" id="post-content" cols="50" rows="30" placeholder="Content"></textarea>
+                                        <textarea class="form-control w-100" name="detail_content" id="post-content" cols="50" rows="30" placeholder="コンテンツ"></textarea>
                                         @error('detail_content')
                                         <b><span style="color: red;">{{ $message }}</span></b>
                                         @enderror
@@ -110,8 +102,8 @@
 
                     </div>
                 </div>
-                <div class="form-group">
-                    <button type="submit" class="button button-contactForm btn_1 boxed-btn">Post</button>
+                <div class="form-group" >
+                    <button type="submit" class="button button-contactForm btn_1 boxed-btn" style="background-color: #C3EF13;">ポースト</button>
                 </div>
             </form>
         </div>
